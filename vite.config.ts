eda7e6eaff387cbb55b168ea7dd93b3734843716
Marin Peșterean <marin.pesterean@carasent.com>
2025-12-15 16:33:03 +0000

    Merged in fix/ASMA-5898-refactor-and-clean-project-2 (pull request #44)
    Fix/ASMA-5898 refactor and clean project 2
    
    * fix: Remove vite-plugin-css-injected-by-js
    
    * v0.0.71

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import terser from '@rollup/plugin-terser'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import * as packageJson from './package.json'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react({
@@ -18,7 +16,6 @@ export default defineConfig({
             insertTypesEntry: true,
             exclude: ['node_modules/**/*', 'src/stories/**', 'src/**/*.stories.tsx', 'src/components/**/makeData.ts'],
         }),
-        cssInjectedByJsPlugin(),
     ],
     build: {
         lib: {
